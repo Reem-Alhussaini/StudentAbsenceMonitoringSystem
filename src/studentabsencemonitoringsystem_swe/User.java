1e64f0ec5aa0ec2d57dbc50bbddb9430ca3ac382
Reem-Alhussaini <rhomoodalhussaini@stu.kau.edu.sa>
2023-11-23 19:36:33 +0300

    constructor

--- src/studentabsencemonitoringsystem_swe/User.java
+++ src/studentabsencemonitoringsystem_swe/User.java
@@ -1,9 +1,12 @@
 package studentabsencemonitoringsystem_swe;
 
+import java.util.logging.Logger;
+
 public class User {
     String F_name;
     String L_name;
     String id;
+    private static final Logger LOG = Logger.getLogger(User.class.getName());
     
     
 }
